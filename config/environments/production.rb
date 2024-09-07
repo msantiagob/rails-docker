578e44296beca4def362e86870cc01e331ba5c04
Santiago Bedoya <santiago@Elrepapu-Mac.local>
2024-09-07 07:56:44 -0500

    working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
-  config.hosts << "rails-docker-production.up.railway.app"
+  config.hosts.clear
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
     .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
